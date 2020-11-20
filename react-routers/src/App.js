767d4f48afcffb519262ab433088cd81e8305720
roylee0704 <roylee0704@gmail.com>
2020-11-20 12:51:17 +0700

    feat: fetch gists data from github

--- react-routers/src/App.js
+++ react-routers/src/App.js
@@ -1,10 +1,33 @@
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello!
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    gists: []
+  }
+
+  componentDidMount() {
+    fetch('https://api.github.com/gists').then(response => response.json()).then(responseData => this.setState({ gists: responseData }))
+  }
+
+  render() {
+    const items = this.state.gists.map(gist => (<li key={gist.id}><a href={gist.url}>{gist.url}</a></li>))
+    return (
+      <div>
+        <section>
+          <ul>
+            {items}
+          </ul>
+        </section>
+
+        <main>
+          Content
+        </main>
+      </div >
+    );
+  }
+
 }
 
 export default App;
