2bedfbc4252f7515d0338d003f71995504f2ed67
roylee0704 <roylee0704@gmail.com>
2020-11-20 15:22:06 +0700

    feat: add router, link and route components

--- react-routers/src/App.js
+++ react-routers/src/App.js
@@ -1,33 +1,75 @@
 import React, { Component } from 'react';
 
 
+// Router: needs to wrap around your app as much as possible... (or at least those that has routes...)
+// Link: to navigate into another url. 
+// Route: to render something when it matches the url
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+
+// https://www.youtube.com/watch?v=cKnc8gXn80Q&feature=emb_logo&t=144s
 class App extends Component {
 
   state = {
-    gists: []
+    gists: null
   }
 
   componentDidMount() {
-    fetch('https://api.github.com/gists').then(response => response.json()).then(responseData => this.setState({ gists: responseData }))
+    fetch('https://api.github.com/gists').then(response => response.json()).then(gists => this.setState({ gists }));
   }
 
   render() {
-    const items = this.state.gists.map(gist => (<li key={gist.id}><a href={gist.url}>{gist.url}</a></li>))
-    return (
-      <div>
-        <section>
-          <ul>
-            {items}
-          </ul>
-        </section>
 
-        <main>
-          Content
-        </main>
-      </div >
+    const { gists } = this.state;
+    console.log(gists)
+
+    const items = gists ?
+      gists.map(gist => (
+        <li key={gist.id}>
+          <Link to={`/g/${gist.id}`}>
+            {gist.description || '[no description]'}
+          </Link>
+        </li>)
+      )
+      :
+      <div>...loading</div>;
+
+    return (
+
+      <Router>
+        <div>
+          <section>
+            <ul>{items}</ul>
+          </section>
+          <main>
+            <Route exact={true} path="/" render={() => (<h1>Welcome</h1>)}></Route>
+
+            {
+              gists && (<Route path="/g/:gistId" render={({ match }) => (
+                <Gist key={match.params.gistId} gist={gists.find(g => g.id === match.params.gistId)} />
+              )} />)
+            }
+          </main>
+        </div >
+      </Router>
+
     );
   }
-
 }
+
+const Gist = ({ gist }) => {
+  return (
+    <div>
+      <h1>{gist.description || '[no description]'}</h1>
+      <ul>
+        {Object.keys(gist.files).map(fileName => (
+          <li key={fileName}>
+            <b>{fileName}</b>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
