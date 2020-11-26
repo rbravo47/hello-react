a8b8e486b091cdcd5c491eae4620343fb60f7434
roylee0704 <roylee0704@gmail.com>
2020-11-26 20:25:14 +0700

    update

--- react-map-with-nasa-api/src/components/Header.js
+++ react-map-with-nasa-api/src/components/Header.js
@@ -5,7 +5,7 @@ import locationIcon from '@iconify/icons-mdi/fire-alert';
 export default function Header() {
     return (
         <div className='header'>
-            <h1><Icon icon={locationIcon} /> Wildfire Tracker (Powered By NASA)</h1>
+            <h1><Icon icon={locationIcon} className="icon" /> Wildfire Tracker (Powered By NASA)</h1>
         </div>
     )
 }
