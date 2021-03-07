28b2b22055b0990b9894b0220275dcadd3aaa169
roylee0704 <roylee0704@gmail.com>
2021-03-07 13:30:24 +0700

    update

--- axios-simple-bootstrap-pagination/README.md
+++ axios-simple-bootstrap-pagination/README.md
@@ -1,7 +1,9 @@
-## Axios Simple Bootstrap Pagination
+## Axios Simple Bootstrap Pagination (Frontend)
 
-
+It fetches all posts(100) at once from http://jsonplaceholder.typicode.com/posts and pagination is done on front-end. The styling is powered by Bootstrap CDN, imported in index.html.
+ 
 
 ### References 
-
-- http://jsonplaceholder.typicode.com/posts
+- Fake REST API, http://jsonplaceholder.typicode.com
+- Bootstrap CDN, https://www.bootstrapcdn.com/.
+- Tutorial: http://jsonplaceholder.typicode.com/posts
--- axios-simple-bootstrap-pagination/src/App.js
+++ axios-simple-bootstrap-pagination/src/App.js
@@ -1,8 +1,45 @@
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import Posts from './components/Posts';
 
-function App() {
+
+const App = () => {
+
+  const [posts, setPosts] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('http://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      console.log(res.data)
+      setLoading(false);
+    }
+
+    fetchPosts();
+  }, []);
+
+  // get current posts
+  // note(roy): index is 1-based - for slice
+  const indexOfLastPost = currentPage * postsPerPage;      // 1 * 10? - this is 1 based index.
+  const indexOfFirstPost = indexOfLastPost - postsPerPage; // 10 - 10 - this is 0 based index.
+
+  console.log(indexOfFirstPost, indexOfLastPost);
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
   return (
-    <div></div>
+    <div className='container'>
+      <h1 className='text-primary mt-5'>My Blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
+    </div>
   );
 }
 
