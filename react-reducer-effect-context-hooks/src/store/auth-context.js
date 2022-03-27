71457be9d9860a322e5086deedbbc9d35572894d
roylee0704 <roylee0704@gmail.com>
2022-03-27 09:31:00 +0800

    refac: auth-context-provider

--- react-reducer-effect-context-hooks/src/store/auth-context.js
+++ react-reducer-effect-context-hooks/src/store/auth-context.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // note(roy): i used kebab naming because pascal is reserved
 // for react component
@@ -6,6 +6,46 @@ import React from "react";
 // note(roy): can be primitive, but usually an object
 const AuthContext = React.createContext({
   isLoggedIn: false,
+
+  // can use dummy for ide auto-completion
+  onLogout: () => {},
+  onLogin: () => {},
 });
+
+// why create AuthContextProvider? to encapsulate state management!
+// in this eg, isLoggedIn
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("isLoggedIn") === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.clear("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogin: loginHandler,
+        onLogout: logoutHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
