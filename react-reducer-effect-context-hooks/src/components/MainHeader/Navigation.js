de642bb4d16ae668811c0bb057fe362dd99034de
roylee0704 <roylee0704@gmail.com>
2022-03-27 09:06:17 +0800

    add cnotext

--- react-reducer-effect-context-hooks/src/components/MainHeader/Navigation.js
+++ react-reducer-effect-context-hooks/src/components/MainHeader/Navigation.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import classes from './Navigation.module.css';
-
+import classes from "./Navigation.module.css";
+import AuthContext from "../../store/auth-context";
 const Navigation = (props) => {
+  console.log(props.isLoggedIn, "el");
+
+  const ctx = useContext(AuthContext);
   return (
     <nav className={classes.nav}>
+      <p>{ctx.isLoggedIn}</p>
+
       <ul>
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <button onClick={props.onLogout}>Logout</button>
           </li>
